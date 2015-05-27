74459850ccb93495d88e8350213f498488050f2d
Giorgio Riccardi <griccardi@my.bcit.ca>
2015-05-26 21:24:31 -0700

    Revert "comment with extra instructions for icon position in the menu"
    This reverts commit e34f85c2a1a5a1bce2dafd714a8286a0884329c5.

--- cpt-and-ct-plugin-news-and-publications.php
+++ cpt-and-ct-plugin-news-and-publications.php
@@ -66,7 +66,7 @@ function my_custom_post_types() {
         'capability_type'    => 'post',
         'has_archive'        => true,
         'hierarchical'       => false,
-        'menu_position'      => 5, //set the position in the dashboard left menu; http://codex.wordpress.org/Function_Reference/register_post_type#Arguments
+        'menu_position'      => 5,
         'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' ),
         //'taxonomies'         => array( 'category', 'post_tag' )   //code-out this line if you want your custom taxonomy to work indipendently from standard posts
     );
